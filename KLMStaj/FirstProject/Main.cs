43c5bcb01001ba79ee4d6c5c9f88506592bba1ad
ebrardemirtass <138399434+ebrardemirtass@users.noreply.github.com>
2023-07-03 16:29:02 +0300

    Update Main.cs

--- KLMStaj/FirstProject/Main.cs
+++ KLMStaj/FirstProject/Main.cs
@@ -1,16 +1,9 @@
-/*
-Console.WriteLine("Hello C#");
-
-Console.WriteLine("Sayı Giriniz:");
-int sayi = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Girdiğiniz Sayi: " + sayi);
-*/
 Console.WriteLine("Sayı Giriniz:");
 bool sonuc;
 int a;
 string sayi= Console.ReadLine();
 sonuc = int.TryParse(sayi, out a);
 if(sonuc)
-    Console.WriteLine("Girdiğiniz Sayi: " + sayi);
+    Console.WriteLine("Girdiğiniz Sayi: " + a);
 else
     Console.WriteLine("Sadece rakamlardan oluşan bir veri giriniz");
