1345e37094124265e7d7245a391c4b4780af9fbd
ebrardemirtass <138399434+ebrardemirtass@users.noreply.github.com>
2023-07-03 18:03:33 +0300

    Update Main.cs

--- KLMStaj/FirstProject/Main.cs
+++ KLMStaj/FirstProject/Main.cs
@@ -1,20 +1,33 @@
-Console.WriteLine("Sayı Giriniz:");
 bool res;
-int number, number2;
-string numberText = Console.ReadLine();
-res = int.TryParse(numberText, out number);
-if (!res)
-     Console.WriteLine("Sadece rakamlardan oluşan bir veri giriniz");
-
-Console.WriteLine("Loop Tekrar Sayısını Giriniz:");
-string numberText2 = Console.ReadLine();
-
-res = int.TryParse(numberText2, out number2);
-if (!res)
-    Console.WriteLine("Sadece rakamlardan oluşan bir veri giriniz");
+int number = 1;
+int number2 = 1;
 int i = 0;
-while( i < number2)
+string numberText = "";
+string numberText2 = "";
+
+while (!(numberText == "0" || numberText2 == "0" || numberText == "EXIT" || numberText2 == "EXIT"))
+{
+    Console.WriteLine("Sayı Giriniz:");
+    numberText = Console.ReadLine();
+
+    res = int.TryParse(numberText, out number);
+    if (!res)
     {
-    Console.WriteLine($"Girdiğiniz Sayı: {number}");
-    i++;
+        Console.WriteLine("Sadece rakamlardan oluşan bir veri giriniz");
+    }
+
+    Console.WriteLine("Loop Tekrar Sayısını Giriniz:");
+    numberText2 = Console.ReadLine();
+
+    res = int.TryParse(numberText2, out number2);
+    if (!res)
+    {
+        Console.WriteLine("Sadece rakamlardan oluşan bir veri giriniz");
+    }
+
+    while (i < number2)
+    {
+        Console.WriteLine($"Girdiğiniz Sayı: {number}");
+        i++;
+    }
 }
