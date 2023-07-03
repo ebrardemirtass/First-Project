13dcdbdc7bfa6e94d55e9da5ec82d15225a5fdf0
ebrardemirtass <138399434+ebrardemirtass@users.noreply.github.com>
2023-07-03 14:52:53 +0300

    Update Main.cs

--- KLMStaj/FirstProject/Main.cs
+++ KLMStaj/FirstProject/Main.cs
@@ -1,5 +1,16 @@
+/*
 Console.WriteLine("Hello C#");
 
 Console.WriteLine("Sayı Giriniz:");
 int sayi = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Girdiğiniz Sayi: " + sayi);
+*/
+Console.WriteLine("Sayı Giriniz:");
+bool sonuc;
+int a;
+string sayi= Console.ReadLine();
+sonuc = int.TryParse(sayi, out a);
+if(sonuc)
+    Console.WriteLine("Girdiğiniz Sayi: " + sayi);
+else
+    Console.WriteLine("Programi Duzgun Kullanin ! ");
