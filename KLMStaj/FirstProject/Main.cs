33d55d2b919be919b587935d37e9f67962c74b7b
ebrardemirtass <138399434+ebrardemirtass@users.noreply.github.com>
2023-07-03 17:07:09 +0300

    Update Main.cs

--- KLMStaj/FirstProject/Main.cs
+++ KLMStaj/FirstProject/Main.cs
@@ -1,9 +1,20 @@
 Console.WriteLine("Sayı Giriniz:");
 bool res;
-int number;
-string numberText= Console.ReadLine();
+int number, number2;
+string numberText = Console.ReadLine();
 res = int.TryParse(numberText, out number);
-if(res)
-    Console.WriteLine("Girdiğiniz Sayi: " + number);
-else
+if (!res)
+     Console.WriteLine("Sadece rakamlardan oluşan bir veri giriniz");
+
+Console.WriteLine("Loop Tekrar Sayısını Giriniz:");
+string numberText2 = Console.ReadLine();
+
+res = int.TryParse(numberText2, out number2);
+if (!res)
     Console.WriteLine("Sadece rakamlardan oluşan bir veri giriniz");
+int i = 0;
+while( i < number2)
+    {
+    Console.WriteLine("Girdiğiniz Sayi: " + number);
+    i++;
+}
