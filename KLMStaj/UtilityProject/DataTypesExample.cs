95da02fc382a979a9d19642ae0b5d8b5399e1926
ebrardemirtass <138399434+ebrardemirtass@users.noreply.github.com>
2023-07-05 12:22:28 +0300

    Update DataTypesExample.cs Used Code Formating

--- KLMStaj/UtilityProject/DataTypesExample.cs
+++ KLMStaj/UtilityProject/DataTypesExample.cs
@@ -1,38 +1,38 @@
-namespace UtilityProject
+int ModeNumber; namespace UtilityProject
 {
-   
-        public class DataTypesExample
-        {
-            public bool MyBool { get; set; }
 
-            public byte MyByte { get; set; }
+    public class DataTypesExample
+    {
+        public bool MyBool { get; set; }
 
-            public sbyte MySByte { get; set; }
+        public byte MyByte { get; set; }
 
-            public char MyChar { get; set; }
+        public sbyte MySByte { get; set; }
 
-            public decimal MyDecimal { get; set; }
+        public char MyChar { get; set; }
 
-            public double MyDouble { get; set; }
+        public decimal MyDecimal { get; set; }
 
-            public float MyFloat { get; set; }
+        public double MyDouble { get; set; }
 
-            public int MyInt { get; set; }
+        public float MyFloat { get; set; }
 
-            public uint MyUInt { get; set; }
+        public int MyInt { get; set; }
 
-            public nint MyNInt { get; set; }
+        public uint MyUInt { get; set; }
 
-            public nuint MyNUInt { get; set; }
+        public nint MyNInt { get; set; }
 
-            public long MyLong { get; set; }
+        public nuint MyNUInt { get; set; }
 
-            public ulong MyULong { get; set; }
+        public long MyLong { get; set; }
 
-            public short MyShort { get; set; }
+        public ulong MyULong { get; set; }
 
-            public ushort MyUShort { get; set; }
+        public short MyShort { get; set; }
 
-        }
-    
+        public ushort MyUShort { get; set; }
+
+    }
+
 }
