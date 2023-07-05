4462244fc949e8e14df33c433f67ef9163046121
ebrardemirtass <138399434+ebrardemirtass@users.noreply.github.com>
2023-07-05 11:14:29 +0300

    Update DateTimeHelper.cs

--- KLMStaj/UtilityProject/DateTimeHelper.cs
+++ KLMStaj/UtilityProject/DateTimeHelper.cs
@@ -1,36 +1,8 @@
 ﻿namespace UtilityProject
 {
-    public class DataTypesExample
-    {
-        public bool MyBool { get; set; }
+    public class DateTimeHelper
+ {
 
-        public byte MyByte { get; set; }
+ }
 
-        public sbyte MySByte { get; set; }
-
-        public char MyChar { get; set; }
-
-        public decimal MyDecimal { get; set; }
-
-        public double MyDouble { get; set; }
-
-        public float MyFloat { get; set; }
-
-        public int MyInt { get; set; }
-
-        public uint MyUInt { get; set; }
-
-        public nint MyNInt { get; set; }
-
-        public nuint MyNUInt { get; set; }
-
-        public long MyLong { get; set; }
-
-        public ulong MyULong { get; set; }
-
-        public short MyShort { get; set; }
-
-        public ushort MyUShort { get; set; }
-
-    }
 }
