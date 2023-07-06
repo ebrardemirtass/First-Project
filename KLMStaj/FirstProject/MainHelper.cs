24a66d771d3418f7fa986cb286e2b7ca6e04b686
ebrardemirtass <ebrar.demirtass@hotmail.com>
2023-07-06 13:52:01 +0300

    MainHelper.cs

---
+++ KLMStaj/FirstProject/MainHelper.cs
@@ -0,0 +1,27 @@
+﻿
+namespace FirstProject
+{
+    internal class MainHelper
+    {
+        public static void GetValidNumber(out int number)
+        {
+            string? enteredText; // nullable
+
+            // Firstly, trying to get valid number
+            do
+            {
+                Console.Write("Sayı Giriniz : ");
+                enteredText = Console.ReadLine();
+                if (!int.TryParse(enteredText, out number))
+                {
+                    Console.WriteLine("Sadece rakamlardan oluşan bir veri giriniz");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            while (!enteredText.Equals("EXIT")); //string string karşılaştırması bütük küçük harf duyarlı
+        }
+    }
+}
