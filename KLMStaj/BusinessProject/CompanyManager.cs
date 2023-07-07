519617a4b95cc0a6415aaf28dfbb1c899ce48f0b
ebrardemirtass <ebrar.demirtass@hotmail.com>
2023-07-07 17:07:00 +0300

    Added BusinessProject

---
+++ KLMStaj/BusinessProject/CompanyManager.cs
@@ -0,0 +1,16 @@
+﻿using ModelProject;
+
+namespace BusinessProject
+{
+    public class CompanyManager
+    {
+        public Company CreateCompany(string name)
+        {
+            Company company = new Company();
+            company.Name = name;
+
+            return company;
+
+        }
+    }
+}
